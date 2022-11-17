5e2482e98077bd5153946a113b80878b23f8fe5b
George Markhulia <george.markhulia@volvocars.com>
2022-11-17 21:54:28 +0100

    Add makefile and dummy service (#1)

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
-	"github.com/ardanlabs/conf"
-	"github.com/dimfeld/httptreemux/v5"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+// import (
+// 	"github.com/ardanlabs/conf"
+// 	"github.com/dimfeld/httptreemux/v5"
+// )
+
+var build = "develop"
+
 func main() {
-	conf.New()
-	httptreemux.New()
+	log.Println("Starting service: ", build)
+	defer log.Println(("Service ended"))
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	log.Println("Stopping service")
 }
