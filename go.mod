089f110f393714e31a5aad4985fad8ebca41f7d4
markhulia <george.markhulia@volvocars.com>
2022-11-17 20:21:37 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/markhulia/go-svc-lab
+
+go 1.19
+
+require (
+	github.com/ardanlabs/conf v1.5.0
+	github.com/dimfeld/httptreemux/v5 v5.5.0
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/ardanlabs/conf"
+	"github.com/dimfeld/httptreemux/v5"
+)
+
+func main() {
+	conf.New()
+	httptreemux.New()
+}
